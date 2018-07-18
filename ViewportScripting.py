bbb28abbb736bc399feb3c44137600211e0c8f69
EmmaTG <grif.emma@gmail.com>
2018-07-18 13:03:27 +0200

    Include image size setValue

--- ViewportScripting.py
+++ ViewportScripting.py
@@ -37,6 +37,7 @@ session.viewports['Viewport: 1'].odbDisplay.setValues(visibleDisplayGroups=(dg, 
 session.viewports['Viewport: 1'].odbDisplay.displayGroupInstances['TestDispGroup'].setValues(
 	lockOptions=OFF)
 session.viewports['Viewport: 1'].odbDisplay.display.setValues(plotState=(CONTOURS_ON_DEF, ))
+session.viewports['Viewport: 1'].odbDisplay.contourOptions(contourStyle=(CONTINUOUS,))
 
 #Switch to Visualization module (??)
 session.mdbData.summary()
@@ -45,6 +46,7 @@ session.viewports['Viewport: 1'].setValues(displayedObject=odb)
 
 # Printing to file
 session.printOptions.setValues(vpDecorations=OFF, reduceColors=False)
+session.pngOptions.setValues(imageSize=(1432,676))
 session.printToFile(
         fileName='/home/cerecam/Desktop/GP_BoundaryConditionTests/Test.png', 
         format=PNG, canvasObjects=(session.viewports['Viewport: 1'], ))
