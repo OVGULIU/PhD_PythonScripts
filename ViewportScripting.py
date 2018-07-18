73020d24f2d49dd1bdc3ebc3429e2598781fbd1f
EmmaTG <grif.emma@gmail.com>
2018-07-18 10:19:10 +0200

    Add scripting from macro to assist with calculating display group data

--- ViewportScripting.py
+++ ViewportScripting.py
@@ -7,19 +7,25 @@ Can also print files directly from script??
 #open modulus, create viewport and open odb
 from abaqus import *
 from abaqusConstants import *
-import displayGroupOdbToolset
+import displayGroupOdbToolset as dgo
 session.Viewport(name='Viewport: 1', origin=(0.0, 0.0), width=268.952117919922,
 height=154.15299987793)
 session.viewports['Viewport: 1'].makeCurrent()
 session.viewports['Viewport: 1'].maximize()
-leafTest = LeafFromElementLabels(partInstanceName='I_Cube', elementLabels = '1,2,4,5,6,7,8:1024')
+leafTest = dgo.LeafFromElementLabels(elementLabels=(('I_Cube',('1:1024')),))
 from caeModules import *
 from driverUtils import executeOnCaeStartup
 executeOnCaeStartup()
 o1 = session.openOdb(name='/home/cerecam/Desktop/GP_BoundaryConditionTests/Flux2_NoUEL.odb')
 session.viewports['Viewport: 1'].setValues(displayedObject=o1)
 ### CREATE OUTPUT ###
-session.DisplayGroup(name='Tester', leaf= leafTest)
+session.viewports['Viewport: 1'].odbDisplay.displayGroup.replace(leaf=leafTest)
+dg = session.viewports['Viewport: 1'].odbDisplay.displayGroup
+dg = session.DisplayGroup(name='TestDispGroup', objectToCopy=dg)
+session.viewports['Viewport: 1'].odbDisplay.setValues(visibleDisplayGroups=(dg, 
+	))
+session.viewports['Viewport: 1'].odbDisplay.displayGroupInstances['TestDispGroup'].setValues(
+	lockOptions=OFF)
 session.viewports['Viewport: 1'].odbDisplay.display.setValues(plotState=(
 CONTOURS_ON_DEF, ))
 session.printToFile(fileName='/home/cerecam/Desktop/GP_BoundaryConditionTests/PrintToFileExample', format=TIFF, canvasObjects=(
