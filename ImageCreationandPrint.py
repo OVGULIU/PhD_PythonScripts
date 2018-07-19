2eb87becd6ce750dabd1ee0293b28111e1fe5d2d
EmmaTG <grif.emma@gmail.com>
2018-07-19 11:08:06 +0200

    Script running, shoing contour and undeformed plot for poly

--- ImageCreationandPrint.py
+++ ImageCreationandPrint.py
@@ -39,21 +39,21 @@ def GoldPrint(DispGroupName):
 	
 def PolyPrint(DispGroupName):
 	session.viewports['Viewport: 1'].enableMultipleColors()
-    session.viewports['Viewport: 1'].setColor(initialColor='#BDBDBD')
-    cmap=session.viewports['Viewport: 1'].colorMappings['Set']
-    session.viewports['Viewport: 1'].setColor(colorMapping=cmap)
-    session.viewports['Viewport: 1'].disableMultipleColors()
-    session.viewports['Viewport: 1'].enableMultipleColors()
-    session.viewports['Viewport: 1'].setColor(initialColor='#BDBDBD')
-    cmap = session.viewports['Viewport: 1'].colorMappings['Set']
-    cmap.updateOverrides(overrides={'I_Cube.Polymer':(True, '#CBCBB6', 'Default', 
-        '#CBCBB6')})
-    session.viewports['Viewport: 1'].setColor(colorMapping=cmap)
-    session.viewports['Viewport: 1'].disableMultipleColors()
+	session.viewports['Viewport: 1'].setColor(initialColor='#BDBDBD')
+	cmap=session.viewports['Viewport: 1'].colorMappings['Set']
+	session.viewports['Viewport: 1'].setColor(colorMapping=cmap)
+	session.viewports['Viewport: 1'].disableMultipleColors()
+	session.viewports['Viewport: 1'].enableMultipleColors()
+	session.viewports['Viewport: 1'].setColor(initialColor='#BDBDBD')
+	cmap = session.viewports['Viewport: 1'].colorMappings['Set']
+	cmap.updateOverrides(overrides={'I_Cube.Polymer':(True, '#CBCBB6', 'Default', 
+	'#CBCBB6')})
+	session.viewports['Viewport: 1'].setColor(colorMapping=cmap)
+	session.viewports['Viewport: 1'].disableMultipleColors()
 	session.viewports['Viewport: 1'].odbDisplay.commonOptions.setValues(
-        renderStyle=FILLED, visibleEdges=NONE, deformationScaling=UNIFORM, 
-        uniformScaleFactor=3.5)
-    if DispGroupName[0].upper()=='X':
+	renderStyle=FILLED, visibleEdges=NONE, deformationScaling=UNIFORM, 
+	uniformScaleFactor=3.5)
+	if DispGroupName[0].upper()=='X':
 		session.viewports['Viewport: 1'].view.setValues(session.views['Right'])	# Set view to the RHS view
 	elif DispGroupName[0].upper()=='Y':
 		session.viewports['Viewport: 1'].view.setValues(session.views['Top'])	# Set view to the Top view
@@ -75,21 +75,21 @@ Keys = []
 reader = csv.reader(csvFile)
 for row in reader:
     Keys.extend(row)
-for DictKey in Keys()[0:3]:
+for DictKey in Keys[0:3]:
 	print(DictKey)
 	#### Display display group in viewport ###
-	dg1= session.displayGroups[DisctKey]
+	dg1= session.displayGroups[DictKey]
 	leaf = dgo.LeafFromDisplayGroup(displayGroup=(dg1, ))
-    session.viewports['Viewport: 1'].odbDisplay.displayGroup.replace(leaf=leaf)
+	session.viewports['Viewport: 1'].odbDisplay.displayGroup.replace(leaf=leaf)
 	if DictKey[-4:].lower() == 'gold':
 		GoldPrint(DictKey)
 	if DictKey[-4:].lower() == 'poly':
 		PolyPrint(DictKey)
 	#session.viewports['Viewport: 1'].odbDisplay.displayGroupInstances[DictKey].setValues(
-		#lockOptions=OFF)
-		
+	#lockOptions=OFF)
+	
 	### Printing to file ###
 	session.printToFile(
-	        fileName='/home/cerecam/Desktop/GP_BoundaryConditionTests/' + DictKey + '.png', 
-	        format=PNG, canvasObjects=(session.viewports['Viewport: 1'], ))
+		fileName='/home/cerecam/Desktop/GP_BoundaryConditionTests/' + DictKey + '.png', 
+		format=PNG, canvasObjects=(session.viewports['Viewport: 1'], ))
 	        
