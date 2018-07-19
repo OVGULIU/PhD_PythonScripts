3fa2b3e8683979725c3817e5d71d3f05a6d966ca
EmmaTG <grif.emma@gmail.com>
2018-07-19 11:00:20 +0200

    ...

--- ImageCreationandPrint.py
+++ ImageCreationandPrint.py
@@ -37,10 +37,21 @@ def GoldPrint(DispGroupName):
 	return
 	
 def PolyPrint(DispGroupName):
-	session.viewports['Viewport: 1'].odbDisplay.display.setValues(plotState=(
-		DEFORMED, CONTOURS_ON_DEF, ))
+	session.viewports['Viewport: 1'].enableMultipleColors()
+    session.viewports['Viewport: 1'].setColor(initialColor='#BDBDBD')
+    cmap=session.viewports['Viewport: 1'].colorMappings['Set']
+    session.viewports['Viewport: 1'].setColor(colorMapping=cmap)
+    session.viewports['Viewport: 1'].disableMultipleColors()
+    session.viewports['Viewport: 1'].enableMultipleColors()
+    session.viewports['Viewport: 1'].setColor(initialColor='#BDBDBD')
+    cmap = session.viewports['Viewport: 1'].colorMappings['Set']
+    cmap.updateOverrides(overrides={'I_Cube.Polymer':(True, '#CBCBB6', 'Default', 
+        '#CBCBB6')})
+    session.viewports['Viewport: 1'].setColor(colorMapping=cmap)
+    session.viewports['Viewport: 1'].disableMultipleColors()
 	session.viewports['Viewport: 1'].odbDisplay.commonOptions.setValues(
-        renderStyle=FILLED, visibleEdges=NO EDGE)
+        renderStyle=FILLED, visibleEdges=NONE, deformationScaling=UNIFORM, 
+        uniformScaleFactor=3.5)
     if DispGroupName[0].upper()=='X':
 		session.viewports['Viewport: 1'].view.setValues(session.views['Right'])	# Set view to the RHS view
 	elif DispGroupName[0].upper()=='Y':
@@ -58,17 +69,19 @@ session.viewports['Viewport: 1'].viewportAnnotationOptions.setValues(triadColor=
 session.viewports['Viewport: 1'].odbDisplay.display.setValues(plotState=(CONTOURS_ON_DEF, ))
         
 ### Creating Display Objects ###
-
+    
 for DictKey in X.keys()[0:3]:
 	print(DictKey)
+	#### Display display group in viewport ###
+	dg1= session.displayGroups[DisctKey]
+	leaf = dgo.LeafFromDisplayGroup(displayGroup=(dg1, ))
+    session.viewports['Viewport: 1'].odbDisplay.displayGroup.replace(leaf=leaf)
 	if DictKey[-4:].lower() == 'gold':
 		GoldPrint(DictKey)
 	if DictKey[-4:].lower() == 'poly':
 		PolyPrint(DictKey)
-	#### Display display group in viewport ###
-	session.viewports['Viewport: 1'].odbDisplay.setValues(visibleDisplayGroups=(dg, ))
-	session.viewports['Viewport: 1'].odbDisplay.displayGroupInstances[DictKey].setValues(
-		lockOptions=OFF)
+	#session.viewports['Viewport: 1'].odbDisplay.displayGroupInstances[DictKey].setValues(
+		#lockOptions=OFF)
 		
 	### Printing to file ###
 	session.printToFile(
